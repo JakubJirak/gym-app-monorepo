17d1c0114bf5bbd32aa58a29f7a71a76ff4fbfe2
Jakub Jirak <jirak.jakub12@seznam.cz>
2025-12-06 23:09:08 +0100

    feat: uprava fontu verze

--- apps/mobile/app/(auth)/(tabs)/profile/about.tsx
+++ apps/mobile/app/(auth)/(tabs)/profile/about.tsx
@@ -10,7 +10,7 @@ export default function About() {
 			<View className="mt-4 flex-row items-center gap-4 rounded-xl bg-secondary p-4">
 				<Info color="white" size={24} />
 				<View className="flex-1 flex-row items-center justify-between">
-					<Text className="font-semibold text-lg text-text">Verze</Text>
+					<Text className="text-lg text-text">Verze</Text>
 					<Text className="text-base text-muted">1.0</Text>
 				</View>
 			</View>
