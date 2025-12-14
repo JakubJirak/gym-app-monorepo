7a58e2532bb648ef598b291a512d15adf2350dca
Jakub Jirak <jirak.jakub12@seznam.cz>
2025-12-14 11:01:12 +0100

    feat: cela serie je tlacitko pro editaci

--- apps/mobile/components/trainings/set.tsx
+++ apps/mobile/components/trainings/set.tsx
@@ -15,23 +15,25 @@ export default function ExerciseSet({ order, weight, reps, isEdit, setId }: Exer
 	const [edit, setEdit] = useState(false);
 
 	return (
-		<View className="my-1 flex-row items-center rounded-xl bg-secondary px-4 py-2.5">
-			<Text className="flex-1 text-text">{order + 1}. série</Text>
-			<Text className="font-semibold text-text">
-				{weight}kg × {reps}
-			</Text>
-			{isEdit && (
-				<TouchableOpacity className="ml-2 rounded-full bg-secondary" onPress={() => setEdit(true)}>
-					<Pencil color="white" size={14} />
-				</TouchableOpacity>
-			)}
-			<EditSetModal
-				defaultReps={reps}
-				defaultWeight={weight}
-				setId={setId}
-				setVisible={setEdit}
-				visible={edit}
-			/>
-		</View>
+		<TouchableOpacity activeOpacity={isEdit ? 0.7 : 1} onPress={() => isEdit && setEdit(true)}>
+			<View className="my-1 flex-row items-center rounded-xl bg-secondary px-4 py-2.5">
+				<Text className="flex-1 text-text">{order + 1}. série</Text>
+				<Text className="font-semibold text-text">
+					{weight}kg × {reps}
+				</Text>
+				{isEdit && (
+					<View className="ml-2 rounded-full bg-secondary">
+						<Pencil color="white" size={14} />
+					</View>
+				)}
+				<EditSetModal
+					defaultReps={reps}
+					defaultWeight={weight}
+					setId={setId}
+					setVisible={setEdit}
+					visible={edit}
+				/>
+			</View>
+		</TouchableOpacity>
 	);
 }
