e1b7c11b41ca5d416acac6449c8b12c65c9f9458
Jakub Jirak <jirak.jakub12@seznam.cz>
2026-03-13 21:53:02 +0100

    fix: odstraneni devtools z prod buildu

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import { devtools } from '@tanstack/devtools-vite'
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
@@ -15,6 +16,7 @@ const config = defineConfig({
 		tanstackStart(),
 		nitro({ preset: "bun" }),
 		viteReact(),
+		devtools(),
 	],
 });
 
