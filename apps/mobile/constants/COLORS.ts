567d1fdceb924c8846afdb35f2d5c0144e89740e
Jakub Jirak <jirak.jakub12@seznam.cz>
2026-04-02 10:11:18 +0200

    feat: uprava barev

--- apps/mobile/constants/COLORS.ts
+++ apps/mobile/constants/COLORS.ts
@@ -1,9 +1,9 @@
 export const COLORS = {
 	primary: "#050505",
-	secondary: "#121212",
+	secondary: "#1a1a1a",
 	border: "#ff944d",
 	accent: "#ed782b",
-	darker: "#0d0d0d",
+	darker: "#0f0f0f",
 	muted: "#8c8c8c",
 	inactive: "#4d4d4d",
 	disabled: "#47230b",
