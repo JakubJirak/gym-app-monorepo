a806ba8e1938f51d6dc16ab975a2435e56325724
Jakub Jirak <jirak.jakub12@seznam.cz>
2026-05-22 23:16:55 +0200

    feat: update novinek a verze aplikace

--- apps/web/src/components/index/News.tsx
+++ apps/web/src/components/index/News.tsx
@@ -3,8 +3,48 @@ const News = () => (
 		className="w-full scroll-mt-20 space-y-12 bg-bg px-6 py-16 md:space-y-20 md:px-20 md:py-28 lg:space-y-26 lg:px-32 lg:py-40"
 		id="novinky"
 	>
-		<div className="mx-auto flex max-w-[75%] flex-col gap-16 xl:max-w-[900px]">
+		<div className="mx-auto flex max-w-[75%] flex-col gap-16 xl:max-w-225">
 			<h3 className="font-bold text-2xl md:text-4xl lg:text-5xl/14">NOVINKY</h3>
+
+			<div className="">
+				<div className="mb-3 flex items-center gap-3 md:mb-4">
+					<div className="size-4 rounded-full bg-orange" />
+					<p className="font-medium text-lg uppercase md:text-xl lg:text-2xl">
+						VERZE 1.2 - 22.5.2026
+					</p>
+				</div>
+
+				<div className="ml-8">
+					<div className="my-4 md:my-5">
+						<p className="mb-2 text-base uppercase md:mb-3 md:text-lg">Mobilní aplikace</p>
+						<ul className="space-y-2 pl-1 md:space-y-2.5">
+							<li className="relative pl-6 text-foreground/90 text-sm leading-relaxed before:absolute before:top-[0.6em] before:left-0 before:size-1.5 before:rounded-full before:bg-primary md:text-base">
+								Nová verze mobilní aplikace s vylepšenými barvami, animacemi a
+								stabilitou.
+							</li>
+						</ul>
+					</div>
+
+					<div className="my-4 md:my-5">
+						<p className="mb-2 text-base uppercase md:mb-3 md:text-lg">Hlavní novinky</p>
+						<ul className="space-y-2 pl-1 md:space-y-2.5">
+							<li className="relative pl-6 text-foreground/90 text-sm leading-relaxed before:absolute before:top-[0.6em] before:left-0 before:size-1.5 before:rounded-full before:bg-primary md:text-base">
+								Sdílení tréninků - Přidání možnosti sdílet trénink s ostatními lidmi bez
+								nutnosti mít účet
+							</li>
+						</ul>
+					</div>
+
+					<div>
+						<p className="mb-2 text-base uppercase md:mb-3 md:text-lg">Ostatí</p>
+						<ul className="space-y-2 pl-1 md:space-y-2.5">
+							<li className="relative pl-6 text-foreground/90 text-sm leading-relaxed before:absolute before:top-[0.6em] before:left-0 before:size-1.5 before:rounded-full before:bg-primary md:text-base">
+								Oprava stability a funkčnosti webové apklikace.
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 
 			<div className="">
 				<div className="mb-3 flex items-center gap-3 md:mb-4">
